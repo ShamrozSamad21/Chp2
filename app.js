b859bae063c848d3e6f41651bbad291f8da03a32
ShamrozSamad21 <shamrozas21@gmail.com>
2024-09-16 03:38:27 +0500

    Chp 2

---
+++ app.js
@@ -0,0 +1,34 @@
+console.log("hello world!")
+
+
+var username = "Shamroz Abdul Samad"
+console.log(username)
+
+
+var myName = "Shamroz Abdul Samad"
+console.log(myName)
+
+
+var message = "Hello World!"
+alert(message)
+
+
+var studentName = "Jhone Doe"
+var studentAge = 15 + " Years"
+var studentCertification = "Certified Mobile Application Development"
+alert(studentName)
+alert(studentAge)
+alert(studentCertification)
+
+
+
+var email = "samadas242@gmail.com"
+alert("my email address is " + email)
+
+
+var book = "A smarter way to learn javascript"
+alert("I am trying to learn from the book " + book)
+
+
+var head = document.querySelector("#head")
+head.innerHTML = "Yah ! I can write HTMl content through JavaScript"
